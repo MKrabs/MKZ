3b355e6c69573718dbfee500acbd0a7143f7a398
Marc <marc.goekce@exxeta.com>
2026-05-18 20:51:17 +0200

    remove: region markers feature.

--- MKZ-website/src/components/map/index.ts
+++ MKZ-website/src/components/map/index.ts
@@ -1,6 +1,5 @@
 export { default as GlobeMap } from './GlobeMap';
 export { MapContext, useMap } from './MapContext';
 export type { MapContextValue } from './MapContext';
-export { default as RegionMarkers } from './RegionMarkers';
 export { RegionMarkersContext, useRegionMarkers } from './RegionMarkersContext';
 export type { PlateMarkerData, RegionMarkersContextValue } from './RegionMarkersContext';
